40b913f8562edef364633d2ad8e769ba60aed4bf
vanxey <82112436+vanxey@users.noreply.github.com>
2025-09-15 20:36:19 +0200

    chore: reorganize folder structure

---
+++ frontend/src/app/(main)/journaling/page.js
@@ -0,0 +1,9 @@
+export default function Journaling() {
+  return (
+    <div className="p-6 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold font-mono">Trade Journal</h1>
+
+      
+    </div>
+  );
+}
