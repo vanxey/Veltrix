72d51dd68fb69998b4058bcb9b6a4c20502ea87f
vanxey <82112436+vanxey@users.noreply.github.com>
2025-09-18 17:21:00 +0200

    feat: add responsive grid layout with burger menu for mobile navigation

--- frontend/src/app/(main)/page.js
+++ frontend/src/app/(main)/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <div className="font-sans grid text-5xl">
+    <div className="font-sans grid">
     <Header />
       <div className="flex font-bold font-mono text-3xl justify-center">Hola</div>
     </div>
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,24 +1,45 @@
+'use client'
+
 import Button from "@/components/ui/button";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header(){
+    const [isMobile, setIsMobile] = useState(false)
 return(
-    <nav>
-        <div className="grid h-16 w-full bg-black text-white grid-rows-1 grid-cols-3 py-4 px-10 m-0">
-            <div className="font-bold text-2xl flex items-center">
-                <Link href="/">Veltrix</Link>
+    <header>
+            <div className="flex h-16 w-full bg-black  text-white justify-between
+            py-4 px-6 m-0">
+                <div className="font-bold text-2xl md:flex items-center">
+                    <Link href="/">Veltrix</Link>
+                </div>
+                <nav className="flex-row gap-4 text-base justify-center items-center hidden md:flex">
+                    <Link href="/">Home</Link>
+                    <Link href="/journaling">Journaling</Link>
+                    <Link href="/analytics">Analytics</Link>
+                    <Link href="/news">News</Link>
+                </nav>
+                <div className="text-base justify-end items-center gap-4 hidden md:flex">
+                    <Link href="/login">Login</Link>
+                    <Button text="Get started"/> 
+                </div>
+                <button
+                onClick={() => setIsMobile(!isMobile)}
+                className="md:hidden text-white focus:outline-none text-2xl"
+                >
+                ☰
+                </button>
+              
             </div>
-            <div className="flex flex-row gap-4 text-base justify-center items-center">
-                <Link href="/">Home</Link>
-                <Link href="/journaling">Journaling</Link>
-                <Link href="/analytics">Analytics</Link>
-                <Link href="/news">News</Link>
-            </div>
-            <div className="text-base flex justify-end items-center gap-4">
-                <Link href="/login">Login</Link>
-                <Button text="Get started"/> 
-            </div>
-        </div>
-    </nav>
+              {isMobile && (
+                    <div className="md:hidden flex flex-col sm:flex-row bg-black border-t border-blue-200 text-white rounded-b-lg place-items-center justify-between">
+                        <div className="hover:bg-blue-600 h-full w-full p-4 rounded-b-lg justify-center flex"><Link href="/">Home</Link></div>
+                        <div className="hover:bg-blue-600 h-full w-full p-4 rounded-b-lg justify-center flex"><Link href="/journaling">Journaling</Link></div>
+                        <div className="hover:bg-blue-600 h-full w-full p-4 rounded-b-lg justify-center flex"><Link href="/analytics">Analytics</Link></div>
+                        <div className="hover:bg-blue-600 h-full w-full p-4 rounded-b-lg justify-center flex"><Link href="/news">News</Link></div>
+                        <div className="hover:bg-blue-600 h-full w-full p-4 rounded-b-lg justify-center flex"><Link href="/login">Login</Link></div>
+                    </div>
+                )}
+    </header>
 )
 }
