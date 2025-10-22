d1089183f37a97cc6497d4d3df6f920d4ee4572f
vanxey <82112436+vanxey@users.noreply.github.com>
2025-10-23 00:51:33 +0200

    feat: add layout for analytics page

--- frontend/src/app/(main)/analytics/page.js
+++ frontend/src/app/(main)/analytics/page.js
@@ -1,9 +1,33 @@
 import Header from "@/components/header"
+import Button from "@/components/ui/button"
 export default function Analytics(){
     return(
-        <div className="grid">
+        <div className="grid grid-rows-[auto_1fr]">
              <Header />
-             <div className="flex font-bold font-mono text-3xl justify-center">Analytics incoming 📊</div>
+             <div className="flex flex-col w-full">
+                <div className="flex bg-gray-200 w-auto h-fit m-5 rounded-xl justify-end">
+
+                    <div className="grid grid-cols-6 gap-2 w-1/2 h-fit justify-center py-2">
+                                    <Button className="text-xs py-0 px-5" text="1W" border/>
+                                    <Button className="text-xs py-0 px-5" text="1M" />
+                                    <Button className="text-xs py-0 px-5" text="3M" border/>
+                                    <Button className="text-xs py-0 px-5" text="6M" border/>
+                                    <Button className="text-xs py-0 px-5" text="1Y" border/>
+                                    <Button className="text-xs py-0 px-5" text="ALL" border/>
+                    </div>
+                </div>
+
+                <div className="flex w-auto h-fit m-5 rounded-xl justify-end">
+
+                    <div className="grid grid-cols-4 gap-2 h-fit w-full justify-center py-2">
+                                    <div className="grid h-50 w-auto bg-gray-200 rounded-xl"></div>
+                                    <div className="grid h-50 w-auto bg-gray-200 rounded-xl"></div>
+                                    <div className="grid h-50 w-auto bg-gray-200 rounded-xl"></div>
+                                    <div className="grid h-50 w-auto bg-gray-200 rounded-xl"></div>
+                    </div>
+                </div>
+             </div>
+             
         </div>
     )
 }
