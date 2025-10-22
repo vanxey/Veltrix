28deb7fccdc82327d8ae5c0c681800dd2fdfc7f1
vanxey <82112436+vanxey@users.noreply.github.com>
2025-10-22 23:22:31 +0200

    fix: implement local env logic again

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const { Pool } = require('pg');
+
+const envFile = process.env.NODE_ENV === 'development' ? '.env.local' : '.env';
+dotenv.config({ path: envFile });
+
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl: envFile
+    ? false
+    : {rejectUnauthorized: false},
 });
 
 module.exports = pool;
